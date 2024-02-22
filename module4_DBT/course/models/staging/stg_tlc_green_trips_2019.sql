f45bf1ae75729202c8a6cb1bc431ca064f94b47f
manon le roux <manonleroux@mailbox.org>
2024-02-22 20:34:37 +0000

    remove get_payment_type_description macro, retry later

--- module4_DBT/course/models/staging/stg_tlc_green_trips_2019.sql
+++ module4_DBT/course/models/staging/stg_tlc_green_trips_2019.sql
@@ -26,7 +26,6 @@ renamed as (
         airport_fee,
         total_amount,
         payment_type,
-        {{ get_payment_type_description("payment_type") }} as payment_type_description,
         distance_between_service,
         time_between_service,
         trip_type,

c344e43ab4777fd901b61430b64932c8c4082ecf
manon le roux <manonleroux@mailbox.org>
2024-02-23 20:25:43 +0000

    green 2019 ok

--- module4_DBT/course/models/staging/stg_tlc_green_trips_2019.sql
+++ module4_DBT/course/models/staging/stg_tlc_green_trips_2019.sql
@@ -1,46 +1,49 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("staging", "tlc_green_trips_2019") }}),
 
-    select * from {{ source('staging', 'tlc_green_trips_2019') }}
+    renamed as (
+        select
+            -- identifiers
+            {{ dbt_utils.generate_surrogate_key(['vendor_id ', 'pickup_datetime']) }} as tripid,
+            {{ dbt.safe_cast("vendor_id ", api.Column.translate_type("integer")) }} as vendor_id ,
+            cast(replace(rate_code,'.0','') as integer) as rate_code,
+            {{ dbt.safe_cast("pickup_location_id", api.Column.translate_type("integer")) }} as pickup_location_id,
+            {{ dbt.safe_cast("dropoff_location_id", api.Column.translate_type("integer")) }} as dropoff_location_id,
 
-),
+            -- timestamps
+            cast(pickup_datetime as timestamp) as pickup_datetime,
+            cast(dropoff_datetime as timestamp) as dropoff_datetime,
 
-renamed as (
+            -- trip info
+            store_and_fwd_flag,
+            {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
+            cast(trip_distance as numeric) as trip_distance,
+            cast(replace(trip_type,'.0','') as integer) as trip_type,
 
-    select
-        {{ dbt_utils.default__generate_surrogate_key(['vendor_id','pickup_datetime']) }} as tripid, 
-        vendor_id,
-        pickup_datetime,
-        dropoff_datetime,
-        store_and_fwd_flag,
-        rate_code,
-        passenger_count,
-        trip_distance,
-        fare_amount,
-        extra,
-        mta_tax,
-        tip_amount,
-        tolls_amount,
-        ehail_fee,
-        airport_fee,
-        total_amount,
-        payment_type,
-        -- coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type_test1, # from the course : Not working, runs fine but replace all values with 0
-        -- cast(payment_type as integer) as payment_type_test2, # Encountered an error: Runtime Error Database Error in sql_operation inline_query (from remote system.sql) Bad int64 value: 2.0
-        -- cast(payment_type as numeric) as payment_type_test3, # works fine but display float in dbt
-        cast(replace(payment_type,'.0','') as integer) as payment_type_test,
-        distance_between_service,
-        time_between_service,
-        trip_type,
-        imp_surcharge,
-        pickup_location_id,
-        dropoff_location_id,
-        data_file_year,
-        data_file_month
+            -- payment info
+            cast(fare_amount as numeric) as fare_amount,
+            cast(extra as numeric) as extra,
+            cast(mta_tax as numeric) as mta_tax,
+            cast(tip_amount as numeric) as tip_amount,
+            cast(tolls_amount as numeric) as tolls_amount,
+            cast(ehail_fee as numeric) as ehail_fee,
+            cast(imp_surcharge as numeric) as imp_surcharge,
+            cast(total_amount as numeric) as total_amount,
+            -- cast(payment_type as integer) as payment_type_test2, # Encountered an error: Runtime Error Database Error in sql_operation inline_query (from remote system.sql) Bad int64 value: 2.0
+            -- cast(payment_type as numeric) as payment_type_test3, # works fine but display float in dbt
+            cast(replace(payment_type,'.0','') as integer) as payment_type_test,
+        from source
+    )
 
-    from source
+select *
+from renamed
 
-)
 
-select * from renamed
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
